71ae5ab38e2ff6e460e2370b0ac48f91868813de
cb57446 <cb57446@gmail.com>
2018-09-04 14:52:07 +0900

    Add argparse to amqp_rpc_client.lua

--- examples/amqp_rpc_client.lua
+++ examples/amqp_rpc_client.lua
@@ -30,10 +30,11 @@
 local amqp = require ('amqp')
 local uuid = require('resty.uuid')
 local inspect = require('inspect')
-
+local argparse = require('argparse')
+--local cqueues = require('cqueues')
 ---------------------------------------------------------------------------------
 -- Main
-
+local parser = argparse()
 --
 -- Instantiate a context
 --
@@ -89,70 +90,92 @@ ctx.opts.callback =
       end
     end
 
-local ok, err
-ok , err = ctx:connect("127.0.0.1",5672)
+local function rpc_client(args)
+  local host = '127.0.0.1'
+  local port = 5672
 
-if not ok then
-  error('could not connect'..err)
+  if ctx.opts.user and args.user then ctx.opts.user = args.user end
+  if ctx.opts.password and args.password then ctx.opts.password = args.password end
+  if ctx.opts.ssl and args.ssl then ctx.opts.ssl = args.ssl end
+  if args.host then host = args.host end
+  if args.port then port = args.port end
+
+  local ok, err
+  ok , err = ctx:connect(host,port)
+
+  if not ok then
+    error('could not connect'..err)
+  end
+
+  --
+  -- Setup connection to AMQP server
+  --
+
+  ok, err = ctx:setup() -- because of this we need to use consume_loop()
+
+  if not ok then
+    error('could not setup: '..err)
+  end
+
+  --
+  -- Prepare to consume
+  --
+
+  ok, err = ctx:prepare_to_consume() -- this has to be right after setup()
+
+  if not ok then
+    error('could not prepare to consume: '..err)
+  end
+
+  --
+  -- Publish ( AKA send command to PING-PONG server in our case )
+  --
+
+  local correlation_id = uuid.generate()
+  local properties = {
+                      reply_to = 'amq.rabbitmq.reply-to',
+                      content_type = 'application/json',
+                      content_encoding = 'utf-8',
+                      correlation_id = correlation_id,
+                      delivery_mode = 2,
+                      headers = { ['api-version'] = 1,
+                                  correlation_id = correlation_id }
+                    }
+
+  local payload = 'ping'
+
+  ok, err = ctx:publish(payload, ctx.opts, properties)
+
+  if not ok then
+    error('could not publish: '..err)
+  end
+
+  print('sent: ping')
+
+  --
+  -- Wait for answer
+  --
+
+  -- Enter consume loop
+  -- callback will be called only on reception of BODY_FRAME.
+  -- setup() and prepare_to_consume() calls are needed because were are using
+  -- consume_loop(callback) method and not consume() method.
+
+  ok = ctx:consume_loop(ctx.opts.callback)
+
+  if not ok then
+    error('could not consume loop: ')
+  end
 end
 
---
--- Setup connection to AMQP server
---
+-- Argument handling ---------------------------------------------------------
+parser:option('-H --host','rabbitmq hostname', '127.0.0.1')
+parser:option('-P --port','rabbitmq port', 5672)
+parser:option('-u --user','rabbitmq username', 'guest')
+parser:option('-p --password','rabbitmq password', 'guest')
+parser:flag('-s --ssl','enable ssl')
 
+local args = parser:parse()
 
-ok, err = ctx:setup() -- because of this we need to use consume_loop()
-
-if not ok then
-  error('could not setup: '..err)
-end
-
---
--- Prepare to consume
---
-
-ok, err = ctx:prepare_to_consume() -- this has to be right after setup()
-
-if not ok then
-  error('could not prepare to consume: '..err)
-end
-
---
--- Publish ( AKA send command to PING-PONG server in our case )
---
-
-local correlation_id = uuid.generate()
-local properties = {
-                     reply_to = 'amq.rabbitmq.reply-to',
-                     content_type = 'application/json',
-                     content_encoding = 'utf-8',
-                     correlation_id = correlation_id,
-                     delivery_mode = 2,
-                     headers = { ['api-version'] = 1,
-                                 correlation_id = correlation_id }
-                   }
-
-local payload = 'ping'
-
-ok, err = ctx:publish(payload, ctx.opts, properties)
-
-if not ok then
-  error('could not publish: '..err)
-end
-
-print('sent: ping')
-
---
--- Wait for answer
---
-
--- Enter consume loop
--- callback will be called only on reception of BODY_FRAME.
--- setup() and prepare_to_consume() calls are needed because were are using
--- consume_loop(callback) method and not consume() method.
-
-ok = ctx:consume_loop(ctx.opts.callback)
-
-if not ok then
-  error('could not consume loop: ')
-end
+print(inspect(args))
+rpc_client(args)
