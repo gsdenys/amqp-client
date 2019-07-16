b7ec410bfe3d00c1d3c71325c1ea17bba31325e1
Denys G. santos <gsdenys@gmail.com>
2019-07-16 11:01:36 -0300

    create publisher.lua

---
+++ examples/publisher.lua
@@ -0,0 +1,33 @@
+local amqp = require "amqp"
+local logger = require "amqp.logger"
+local uuid = require "uuid"
+
+local host = "127.0.0.1"
+local port = 5672
+
+logger.set_level(7)
+
+local ctx = amqp:new(
+    {
+        role = 'producer',
+        exchange = 'amq.topic',
+        --queue = '',
+        routing_key = 'test',
+        ssl = false,
+        user = 'guest',
+        password = 'guest',
+        no_ack = false,
+        durable = true,
+        auto_delete = true,
+        consumer_tag = '',
+        exclusive = false,
+        properties = {}
+    }
+)
+    
+local ok1, err1 = ctx:connect(host, port)
+local ok2, err2 = ctx:setup()
+local ok3, err3 = ctx:publish("Hello world!",{},{correlation_id = uuid()})
+
+ctx:teardown()
+ctx:close()
